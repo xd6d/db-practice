3cc179eae2e560dfdf74141f872c75366764786d
xd6d <kurkin566@gmail.com>
2023-12-22 15:37:00 +0200

    fixed pom.xml

--- src/main/java/com/solvd/laba/hospital/dao/Config.java
+++ src/main/java/com/solvd/laba/hospital/dao/Config.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
