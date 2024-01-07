52cac5612d560a71be6422af3982707a3645df87
xd6d <kurkin566@gmail.com>
2024-01-07 22:51:56 +0200

    done JAXB

--- src/main/java/com/solvd/laba/hospital/model/info/Allergy.java
+++ src/main/java/com/solvd/laba/hospital/model/info/Allergy.java
@@ -1,11 +1,16 @@
 package com.solvd.laba.hospital.model.info;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(of = "id")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Allergy {
+    @XmlAttribute
     private long id;
     private String name;
 }
--- src/main/java/com/solvd/laba/hospital/model/person/Person.java
+++ src/main/java/com/solvd/laba/hospital/model/person/Person.java
@@ -1,11 +1,16 @@
 package com.solvd.laba.hospital.model.person;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(of = "id")
+@XmlAccessorType(XmlAccessType.FIELD)
 public abstract class Person {
+    @XmlAttribute
     private long id;
     private String name;
     private String surname;
